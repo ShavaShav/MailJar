231ad320d3cef5e7014ab869399e3a195c74b9d1
kimficara <kim.ficara@gmail.com>
2017-03-12 00:25:28 -0500

    Update MessageWindow.java

--- src/app/view/MessageWindow.java
+++ src/app/view/MessageWindow.java
@@ -1,7 +1,22 @@
 package app.view;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class MessageWindow extends Stage {
+	
+	AnchorPane root;
+	
+	public MessageWindow(){
+		
+		root = new AnchorPane();
+		
+		
+		
+		// set stage and show
+		this.setScene(new Scene(root, 800, 500));
+		this.show();
+	}
 
 }
