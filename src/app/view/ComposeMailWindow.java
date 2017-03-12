f4bd2a08294b949398f3b2e32ea83d250a1c170a
kimficara <kim.ficara@gmail.com>
2017-03-12 00:24:44 -0500

    Update ComposeMailWindow.java

--- src/app/view/ComposeMailWindow.java
+++ src/app/view/ComposeMailWindow.java
@@ -1,7 +1,22 @@
 package app.view;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ComposeMailWindow extends Stage{
+	
+	AnchorPane root;
+	
+	public ComposeMailWindow(){
+		
+		root = new AnchorPane();
+		
+		
+		
+		// set stage and show
+		this.setScene(new Scene(root, 800, 500));
+		this.show();
+	}
 
 }
