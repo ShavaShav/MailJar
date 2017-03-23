92b512aca0f114a392a62600550137d8577df308
Zach Shaver <zachshaver@gmail.com>
2017-03-23 04:04:34 -0400

    starting to parse messages

--- src/app/model/MailboxModel.java
+++ src/app/model/MailboxModel.java
@@ -1,5 +1,13 @@
 package app.model;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Properties;
 import java.util.regex.Pattern;
@@ -8,6 +16,7 @@ import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
+import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.Transport;
@@ -104,6 +113,91 @@ public class MailboxModel {
 	
 	public Store getStore(){ return store; }
 	public Session getSession(){ return emailSession; }
+	
+//	public String getStringFromMessage(Message message){
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//	}
+	
+	public static void writePart(Part p) throws Exception {
+		if (p instanceof Message)	
+		System.out.println("----------------------------");
+		System.out.println("CONTENT-TYPE: " + p.getContentType());
+	
+		//check if the content is plain text
+		if (p.isMimeType("text/plain")) {
+			System.out.println("This is plain text");
+			System.out.println("---------------------------");
+			System.out.println((String) p.getContent());
+		} 
+		//check if the content has attachment
+		else if (p.isMimeType("multipart/*")) {
+			System.out.println("This is a Multipart");
+			System.out.println("---------------------------");
+			Multipart mp = (Multipart) p.getContent();
+			int count = mp.getCount();
+			for (int i = 0; i < count; i++)
+				writePart(mp.getBodyPart(i));
+		} 
+		//check if the content is a nested message
+		else if (p.isMimeType("message/rfc822")) {
+			System.out.println("This is a Nested Message");
+			System.out.println("---------------------------");
+			writePart((Part) p.getContent());
+		} 
+		//check if the content is an inline image
+		else if (p.isMimeType("image/jpeg")) {
+			System.out.println("--------> image/jpeg");
+			Object o = p.getContent();
+	
+			InputStream x = (InputStream) o;
+			// Construct the required byte array
+			System.out.println("x.length = " + x.available());
+			int i = 0;
+			byte[] bArray = new byte[x.available()];
+			while ((i = (int) ((InputStream) x).available()) > 0) {
+				int result = (int) (((InputStream) x).read(bArray));
+				if (result == -1)
+					break;
+			}
+			FileOutputStream f2 = new FileOutputStream("/tmp/image.jpg");
+			f2.write(bArray);
+		} 
+		else if (p.getContentType().contains("image/")) {
+			System.out.println("content type" + p.getContentType());
+			File f = new File("image" + new Date().getTime() + ".jpg");
+			DataOutputStream output = new DataOutputStream(
+					new BufferedOutputStream(new FileOutputStream(f)));
+	        com.sun.mail.util.BASE64DecoderStream test = 
+	             (com.sun.mail.util.BASE64DecoderStream) p.getContent();
+	        byte[] buffer = new byte[1024];
+	        int bytesRead;
+	        while ((bytesRead = test.read(buffer)) != -1) {
+	        	output.write(buffer, 0, bytesRead);
+	        }
+		} 
+		else {
+			Object o = p.getContent();
+			if (o instanceof String) {
+				System.out.println("This is a string");
+				System.out.println("---------------------------");
+				System.out.println((String) o);
+			} 
+			else if (o instanceof InputStream) {
+				System.out.println("This is just an input stream");
+				System.out.println("---------------------------");
+				InputStream is = (InputStream) o;
+				is = (InputStream) o;
+				int c;
+				while ((c = is.read()) != -1)
+					System.out.write(c);
+			} 
+			else {
+				System.out.println("This is an unknown type");
+				System.out.println("---------------------------");
+	            System.out.println(o.toString());
+	 		}
+	  	}
+	}
 	
 	public static Multipart getMultipartFromMessage(Message message) throws Exception{
 		Object content = message.getContent(); // throws I/O excep
