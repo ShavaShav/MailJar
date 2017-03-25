617cb60d4e327feeef4146737fec3e23917bc8a9
Zach Shaver <zachshaver@gmail.com>
2017-03-25 08:43:34 -0400

    reverted

--- src/app/view/LoginWindow.java
+++ src/app/view/LoginWindow.java
@@ -1,7 +1,6 @@
 package app.view;
 
 import app.model.MailboxModel;
-import app.model.SMTPModel;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -63,7 +62,7 @@ public class LoginWindow extends Stage implements EventHandler<ActionEvent> {
 		passwordField = new PasswordField();
 		passwordField.setText("agileasFUCK");
 		passwordField.setPrefWidth(200.0);
-		promptText = new Text();
+		promptText = new Text("");
 		promptText.setWrappingWidth(390.00);
 		promptText.setId("prompt");
 		loginBtn = new Button("Sign-in");
@@ -71,8 +70,24 @@ public class LoginWindow extends Stage implements EventHandler<ActionEvent> {
 		loginBtn.getStyleClass().add("buttonClass");
 		hostBox = new ComboBox<String>();
 		hostBox.getItems().addAll("gmail.com", "hotmail.com");
-
-		// add action handlers	       
+		
+		//add action handlers
+		userField.setOnAction(this); // set to LoginWindow's action handler
+		hostBox.valueProperty().addListener(new ChangeListener<String>(){
+			@Override
+			public void changed(ObservableValue<? extends String> comboBox, String lastSelection, String currentSelection) {
+				host = currentSelection;
+			}	
+		});
+		
+		passwordField.setOnAction(this); // set to LoginWindow's action handler
+		hostBox.valueProperty().addListener(new ChangeListener<String>(){
+			@Override
+			public void changed(ObservableValue<? extends String> comboBox, String lastSelection, String currentSelection) {
+				host = currentSelection;
+			}	
+		});
+      
 		loginBtn.setOnAction(this); // set to LoginWindow's action handler
 		hostBox.valueProperty().addListener(new ChangeListener<String>(){
 			@Override
@@ -123,7 +138,7 @@ public class LoginWindow extends Stage implements EventHandler<ActionEvent> {
 		AnchorPane.setLeftAnchor(promptText, 10.0);	
 
 		// add nodes to root scene
-		root.getChildren().addAll(mainPart, loginBtn, promptText);
+		root.getChildren().addAll(mainPart, promptText, loginBtn);
 		// import css
 		root.getStylesheets().add("app/view/common.css");
 		root.getStylesheets().add("app/view/LoginWindowStyles.css");
@@ -137,18 +152,18 @@ public class LoginWindow extends Stage implements EventHandler<ActionEvent> {
 	public void handle(ActionEvent arg0) {
 		String email = userField.getText() + '@' + host;
 		String password = passwordField.getText();
+	
 		// try to login
 		try {
-			SMTPModel smtp = new SMTPModel(email, password);
 			MailboxModel mailbox = new MailboxModel(email, password);
 			// if successful, create main window and close this one
-			new MailboxWindow(mailbox, smtp);
+			new MailboxWindow(mailbox);
 			this.close();
 		} catch (Exception e) {
-			// show user error message so they can correct
-			e.printStackTrace();
+			// show user the error message so they can correct
 			promptText.setText(e.getMessage());
-		} 
+			
+		}
 	}
 }
 
